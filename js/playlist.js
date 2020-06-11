f9728a2350601559765bb718c57044bffcbd8d0c
Kiara Anderson <kiara@Kiaras-MacBook-Air.local>
2020-06-11 18:12:45 -0300

    player y playlist casi hecho

--- js/playlist.js
+++ js/playlist.js
@@ -1,2 +1,26 @@
 let recuperoStorage = localStorage.getItem("playlist");
 let playlist = JSON.parse(recuperoStorage);
+let playlistWrapper = document.querySelector('.playlistwrapper');
+let player = document.querySelector('iframe');
+
+playlist.forEach(function(idtrack){
+    mostrarTrack(idtrack);
+})
+
+function mostrarTrack(idtrack){
+    let proxy = "https://cors-anywhere.herokuapp.com/";
+    let url = proxy + "https://api.deezer.com/track/" + idtrack;
+
+    fetch(url)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(track){
+            console.log(track);
+            playlistWrapper.innerHTML += "<li class='cadatrack'><a href='detalle.html?type=" + track.type + "&id=" + track.id + "' class='links'> Cancion: " + track.title + "</a><a href='detalle.html?type=" + track.artist.type + "&id=" + track.artist.id + "' class='links'>  - Artista: " + track.artist.name + "</a><a href='detalle.html?type=" + track.album.type + "&id=" + track.album.id + "' class='links'>  - Album: " + track.album.title + "</a></li>";
+            player.src = 'https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + idtrack + '&app_id=1';
+        })
+        .catch(function(error){
+            console.log(error);
+    })
+}

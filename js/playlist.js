fb3a4e82c6f58cc1c8b41bf6a074a9b3644d6bda
Kiara Anderson <kiara@Kiaras-MacBook-Air.local>
2020-06-11 19:28:14 -0300

    players

--- js/playlist.js
+++ js/playlist.js
@@ -17,7 +17,13 @@ function mostrarTrack(idtrack){
         })
         .then(function(track){
             console.log(track);
-            playlistWrapper.innerHTML += "<li class='cadatrack'><a href='detalle.html?type=" + track.type + "&id=" + track.id + "' class='links'> Cancion: " + track.title + "</a><a href='detalle.html?type=" + track.artist.type + "&id=" + track.artist.id + "' class='links'>  - Artista: " + track.artist.name + "</a><a href='detalle.html?type=" + track.album.type + "&id=" + track.album.id + "' class='links'>  - Album: " + track.album.title + "</a></li> <iframe scrolling='no' frameborder='0' allowTransparency='true' src='https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id='" + idtrack + "'&app_id=1' width='500' height='90'></iframe>";
+            let x = window.matchMedia("(min-width: 768px)");
+            if(x.matches){
+                playlistWrapper.innerHTML += "<li class='cadatrack'><a href='detalle.html?type=" + track.type + "&id=" + track.id + "' class='links'> Cancion: " + track.title + "</a><a href='detalle.html?type=" + track.artist.type + "&id=" + track.artist.id + "' class='links'>  - Artista: " + track.artist.name + "</a><a href='detalle.html?type=" + track.album.type + "&id=" + track.album.id + "' class='links'>  - Album: " + track.album.title + "</a></li> <iframe scrolling='no' frameborder='0' allowTransparency='true' src='https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id='" + idtrack + "'&app_id=1' width='500' height='90'></iframe>";
+            } else {
+                playlistWrapper.innerHTML += "<li class='cadatrack'><a href='detalle.html?type=" + track.type + "&id=" + track.id + "' class='links'>" + track.title + "</a><br><a href='detalle.html?type=" + track.artist.type + "&id=" + track.artist.id + "' class='links'>" + track.artist.name + "</a><a href='detalle.html?type=" + track.album.type + "&id=" + track.album.id + "' class='links'>  - " + track.album.title + "</a></li> <iframe scrolling='no' frameborder='0' allowTransparency='true' src='https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=200&height=200&color=007FEB&layout=dark&size=medium&type=tracks&id=" + idtrack + "'&app_id=1' width='200' height='200'></iframe>";
+            }
+            
         })
         .catch(function(error){
             console.log(error);

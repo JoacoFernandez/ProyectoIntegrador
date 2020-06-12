bcee0f59bcf03b57b1397f49367d676b835b39e0
Kiara Anderson <kiara@Kiaras-MacBook-Air.local>
2020-06-12 14:15:33 -0300

    acorte js detalle

--- js/detalle.js
+++ js/detalle.js
@@ -25,177 +25,129 @@ let proxy = "https://cors-anywhere.herokuapp.com/";
 let url = proxy + "https://api.deezer.com/" + type + "/" + id;
 
 let body = document.querySelector(".section");
+let imagen = document.querySelector(".imagen");
+let x = window.matchMedia("(max-width: 768px)");         
+let info = document.querySelector(".informacion");
 
-
-if(type == "artist"){
-    fetch(url)
+fetch(url)
     .then(function(response){
         return response.json();
     })
     .then(function(datos){
-        console.log(datos);
-        let imagen = document.querySelector(".imagen");
-        let x = window.matchMedia("(max-width: 768px)");
-        if(x.matches){
-            imagen.innerHTML += '<img src="' + datos.picture_medium +  '" class="foto" alt="">';
-        } else {
-            imagen.innerHTML += '<img src="' + datos.picture_big +  '" class="foto" alt="">';
-        }
-        
-
-        let info = document.querySelector(".informacion");
-        info.innerHTML += "<h2> Nombre: " + datos.name + "</h2>";
-        info.innerHTML += "<h2> Albums: " + datos.nb_album + "</h2>";
-        info.innerHTML += "<h2> Fans: " + datos.nb_fan + "</h2>";
-
-        let toptracks = proxy + datos.tracklist;
-        fetch(toptracks)
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(infotracks){
-                console.log(infotracks);
-                info.innerHTML += "<h2>Top Canciones:</h2>";
-                info.innerHTML += "<ol class='lista'></ol>";
-                let lista = document.querySelector(".lista");
-                let itemslista = infotracks.data;
-                for(let i=0; i<5; i++){
-                    lista.innerHTML += "<li><a href='detalle.html?type=" + itemslista[i].type + "&id=" + itemslista[i].id + "' class='links'>" + itemslista[i].title + "</a></li>";
+        switch(type){
+            case ('artist'):
+                if(x.matches){
+                    imagen.innerHTML += '<img src="' + datos.picture_medium +  '" class="foto" alt="">';
+                } else {
+                    imagen.innerHTML += '<img src="' + datos.picture_big +  '" class="foto" alt="">';
                 }
-            })
-            .catch(function(error){
-                console.log(error);
-        })
-    })
-    .catch(function(error){
-        console.log(error);
-})
-}else if(type == "album"){
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
-        console.log(datos);
-        let imagen = document.querySelector(".imagen");
-        
-        let x = window.matchMedia("(max-width: 768px)");
-        if(x.matches){
-            imagen.innerHTML += '<img src="' + datos.cover_medium +  '" class="foto" alt="">';
-        } else {
-            imagen.innerHTML += '<img src="' + datos.cover_big +  '" class="foto" alt="">';
-        }
+                info.innerHTML += "<h2> Nombre: " + datos.name + "</h2>";
+                info.innerHTML += "<h2> Albums: " + datos.nb_album + "</h2>";
+                info.innerHTML += "<h2> Fans: " + datos.nb_fan + "</h2>";
 
-        let info = document.querySelector(".informacion");
-        info.innerHTML += "<h2>Nombre: " + datos.title + "</h2>";
-        info.innerHTML += "<h2><a href='detalle.html?type=" + datos.artist.type + "&id=" + datos.artist.id + "' class='links'>" + "Artista: " + datos.artist.name + "</a></h2>";
-        info.innerHTML += "<h2>Fecha de salida: " + datos.release_date + "</h2>";
-        let tracks = proxy + datos.tracklist;
-        fetch(tracks)
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(tracks){
-                console.log(tracks);
-                info.innerHTML += "<h2>Canciones:</h2>";
-                info.innerHTML += "<ol class='lista'></ol>";
-                let lista = document.querySelector(".lista");
-                let itemslista = tracks.data;
-                itemslista.forEach(function(result){
-                    lista.innerHTML += "<li><a href='detalle.html?type=" + result.type + "&id=" + result.id + "' class='links'>" + result.title + "</a></li>";
+                let toptracks = proxy + datos.tracklist;
+                fetch(toptracks)
+                    .then(function(response){
+                        return response.json();
+                    })
+                    .then(function(infotracks){
+                        console.log(infotracks);
+                        info.innerHTML += "<h2>Top Canciones:</h2>";
+                        info.innerHTML += "<ol class='lista'></ol>";
+                        let lista = document.querySelector(".lista");
+                        let itemslista = infotracks.data;
+                        for(let i=0; i<5; i++){
+                            lista.innerHTML += "<li><a href='detalle.html?type=" + itemslista[i].type + "&id=" + itemslista[i].id + "' class='links'>" + itemslista[i].title + "</a></li>";
+                        }
+                    })
+                    .catch(function(error){
+                    console.log(error);
                 })
-            
-                
-            })
-            .catch(function(error){
-                console.log(error);
-        })
-    })
-    .catch(function(error){
-        console.log(error);
-})
+            break;
 
-}else if(type == "track"){
-    fetch(url)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(datos){
-        console.log(datos);
-        let imagen = document.querySelector(".imagen");
-        let x = window.matchMedia("(max-width: 768px)");
-        if(x.matches){
-            imagen.innerHTML += '<img src="' + datos.album.cover_medium +  '" class="foto" alt="">';
-        } else {
-            imagen.innerHTML += '<img src="' + datos.album.cover_big +  '" class="foto" alt="">';
+            case ('album'):
+                if(x.matches){
+                    imagen.innerHTML += '<img src="' + datos.cover_medium +  '" class="foto" alt="">';
+                } else {
+                    imagen.innerHTML += '<img src="' + datos.cover_big +  '" class="foto" alt="">';
+                }
+                info.innerHTML += "<h2>Nombre: " + datos.title + "</h2>";
+                info.innerHTML += "<h2><a href='detalle.html?type=" + datos.artist.type + "&id=" + datos.artist.id + "' class='links'>" + "Artista: " + datos.artist.name + "</a></h2>";
+                info.innerHTML += "<h2>Fecha de salida: " + datos.release_date + "</h2>";
+                let tracks = proxy + datos.tracklist;
+                fetch(tracks)
+                    .then(function(response){
+                        return response.json();
+                    })
+                    .then(function(tracks){
+                        console.log(tracks);
+                        info.innerHTML += "<h2>Canciones:</h2>";
+                        info.innerHTML += "<ol class='lista'></ol>";
+                        let lista = document.querySelector(".lista");
+                        let itemslista = tracks.data;
+                        itemslista.forEach(function(result){
+                            lista.innerHTML += "<li><a href='detalle.html?type=" + result.type + "&id=" + result.id + "' class='links'>" + result.title + "</a></li>";
+                        })
+                    
+                        
+                    })
+                    .catch(function(error){
+                        console.log(error);
+                })
+            break;
+
+            case ('track'):
+                if(x.matches){
+                    imagen.innerHTML += '<img src="' + datos.album.cover_medium +  '" class="foto" alt="">';
+                } else {
+                    imagen.innerHTML += '<img src="' + datos.album.cover_big +  '" class="foto" alt="">';
+                }
+                info.innerHTML += "<h2>Nombre: " + datos.title + "</h2>";
+                info.innerHTML += "<h2><a href='detalle.html?type=" + datos.artist.type + "&id=" + datos.artist.id + "' class='links'>" + "Artista: " + datos.artist.name + "</a></h2>";
+                info.innerHTML += "<h2><a href='detalle.html?type=" + datos.album.type + "&id=" + datos.album.id + "' class='links'>" + "Album: " + datos.album.title + "</a></h2>";
+                info.innerHTML += "<h2> Fecha de salida: " + datos.release_date + "</h2>";
+                info.innerHTML += "<h2>Duracion: " + datos.duration + " segundos</h2>";
+                info.innerHTML += '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + id + '&app_id=1" width="300" height="90"></iframe>';
+                info.innerHTML += "<a href='#' class='boton'> Agregar a la playlist </a>";
+        
+                let recuperoStorage = localStorage.getItem('playlist');
+                if(recuperoStorage == null){
+                    playlist = [];
+                } else {
+                    playlist = JSON.parse(recuperoStorage);
+                }
+        
+                let boton = document.querySelector('.boton');
+                if(playlist.includes(id)){
+                    boton.innerHTML = "Quitar de la playlist";
+                }
+        
+                boton.addEventListener('click', function(e){
+                    e.preventDefault();
+        
+                    if(playlist.includes(id)){
+                        let indiceArray = playlist.indexOf(id);
+                        playlist.splice(indiceArray, 1);
+                        boton.innerHTML = "Agregar a la playlist";
+                        console.log(playlist);
+                    } else{
+                        playlist.push(id);
+                        boton.innerHTML = "Quitar de la playlist";
+                    }
+                    
+                    let playlistParaStorage = JSON.stringify(playlist);
+                    localStorage.setItem('playlist', playlistParaStorage);
+                    console.log(localStorage);
+                })
+            break;
+
+            case ('genre'):
+                if(x.matches){
+                    imagen.innerHTML += '<img src="' + datos.picture_medium +  '" class="foto" alt="">';
+                } else {
+                    magen.innerHTML += '<img src="' + datos.picture_big +  '" class="foto" alt="">';
+                }
+                info.innerHTML += "<h2>Nombre: " + datos.name + "</h2>";
+            break;
         }
-
-        let info = document.querySelector(".informacion");
-        info.innerHTML += "<h2>Nombre: " + datos.title + "</h2>";
-        info.innerHTML += "<h2><a href='detalle.html?type=" + datos.artist.type + "&id=" + datos.artist.id + "' class='links'>" + "Artista: " + datos.artist.name + "</a></h2>";
-        info.innerHTML += "<h2><a href='detalle.html?type=" + datos.album.type + "&id=" + datos.album.id + "' class='links'>" + "Album: " + datos.album.title + "</a></h2>";
-        info.innerHTML += "<h2> Fecha de salida: " + datos.release_date + "</h2>";
-        info.innerHTML += "<h2>Duracion: " + datos.duration + " segundos</h2>";
-        info.innerHTML += '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="https://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + id + '&app_id=1" width="300" height="90"></iframe>';
-        info.innerHTML += "<a href='#' class='boton'> Agregar a la playlist </a>";
-
-        let recuperoStorage = localStorage.getItem('playlist');
-        if(recuperoStorage == null){
-            playlist = [];
-        } else {
-            playlist = JSON.parse(recuperoStorage);
-        }
-
-        let boton = document.querySelector('.boton');
-        if(playlist.includes(id)){
-            boton.innerHTML = "Quitar de la playlist";
-        }
-
-        boton.addEventListener('click', function(e){
-            e.preventDefault();
-
-            if(playlist.includes(id)){
-                let indiceArray = playlist.indexOf(id);
-                playlist.splice(indiceArray, 1);
-                boton.innerHTML = "Agregar a la playlist";
-                console.log(playlist);
-            } else{
-                playlist.push(id);
-                boton.innerHTML = "Quitar de la playlist";
-            }
-            
-            let playlistParaStorage = JSON.stringify(playlist);
-            localStorage.setItem('playlist', playlistParaStorage);
-            console.log(localStorage);
-        })
-
     })
-    .catch(function(error){
-        console.log(error);
-    })
-
-    
-
-
-} else if(type == "genre"){
-    fetch(url)
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(datos){
-            console.log(datos);
-            let imagen = document.querySelector(".imagen");
-            let x = window.matchMedia("(max-width: 768px)");
-            if(x.matches){
-                imagen.innerHTML += '<img src="' + datos.picture_medium +  '" class="foto" alt="">';
-            } else {
-                imagen.innerHTML += '<img src="' + datos.picture_big +  '" class="foto" alt="">';
-            }
-
-            let info = document.querySelector(".informacion");
-            info.innerHTML += "<h2>Nombre: " + datos.name + "</h2>";
-        })
-        .catch(function(error){
-            console.log(error);
-    })
-}

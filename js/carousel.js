7ea3516d8f7b4a7ff6decc4de1b7029a93e0ca82
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-12 11:56:28 -0300

    java en carousel conectando

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+const track = document.querySelector('.carouseltrack');
+const slides = Array.from(track.children);
+const nextButton = document.querySelector('carousel__button--right');
+const prevButton = document.querySelector('carousel__button--left');
+const dotsNavs = document.querySelector('carouselnav');
+const dotsNavs = Array.from(dotsNav.children);
+
+
+//cuando aprieto para la izq que se mueva, y vice versa
+//nav indicator//

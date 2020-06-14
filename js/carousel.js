975063a672f35c059a2ee025e2feee259cb0d150
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-14 00:02:51 -0300

    carousel casi terminado

--- js/carousel.js
+++ js/carousel.js
@@ -16,29 +16,59 @@ const setSlidePosition = (slide, index) => {
 slides.forEach(setSlidePosition);
 
 const moveToSlide = (track, currentSlide, targetSlide) => {
-    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';
+    track.style.transform = 'translateX(-' + targetSlide.style.left +')';
     currentSlide.classList.remove('current-slide');
     targetSlide.classList.add('current-slide');
 }
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targeIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targeIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide);
+    updateDots(currentDot, nextDot);
+    hideShowArrows(slides, prevButton, nextButton, nextIndex);
+
+//move to next slide//
+ });
 
 
 prevButton.addEventListener('click' , e => {
 const currentSlide = track.querySelector('.current-slide');
 const prevSlide = currentSlide.previousElementSibling;
+const currentDot = dotsNav.querySelector('.current-slide');
+const prevDot = currentDot.previousElementSibling;
+const prevIndex = slides.findIndex(slide => slide === prevSlide);
 
 moveToSlide(track, currentSlide, prevSlide);
+updateDots(currentDot, prevDot);
+hideShowArrows(slides, prevButton, nextButton, nextIndex);
 });
 
 //cuando aprieto para la izq que se mueva, y vice versa
 //nav indicator//
 
-nextButton.addEventListener('click', e => {
-    const currentSlide = track.querySelector('.current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-  
-    moveToSlide(track, currentSlide, nextSlide);
-//move to next slide//
- });
 
 //si usas un class list sin punto, si usas query con punto//
 
@@ -46,7 +76,8 @@ dotsNav.addEventListener('click', e => {
    //que indicador apretamos//
     const targetDot = e.target.closest('button');
 
-    if (!targetDot) return; //si algo en la funcion es falso, el return para la funcion//
+    if (!targetDot) return; 
+    //si algo en la funcion es falso, el return para la funcion//
   
     const currentSlide = track.querySelector('.current-slide');
     const currentDot = dotsNav.querySelector('.current-slide');
@@ -54,20 +85,9 @@ dotsNav.addEventListener('click', e => {
     const targetSlide = slides[targetIndex];
 
     moveToSlide(track, currentSlide, targetSlide);
-
-    currentDot.classList.remove('current-slide');
-    targetDot.classList.add('current-slide')
-
-    if (targetIndex === 0) {
-        prevButton.classList.add('is-hidden');
-        nextButton.classList.remove('is-hidden');
-    } else if (targeIndex === slides.length - 1 ) {
-        prevButton.classList.remove('is-hidden');
-        nextButton.classList.add('is-hidden');
-    } else {
-        prevButton.classList.remove('is-hidden');
-        nextButton.classList.remove('is-hidden');
-    }
+    updateDots(currentDot, targetDot);
+    hideShowArrows = (slides, prevButton, nextButton, targeIndex);
+  
     
     
 })

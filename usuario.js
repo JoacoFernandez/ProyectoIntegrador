84b9b8ffb745aaff5d6e609a0f3d248f3fa664e6
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-14 23:18:49 -0300

    pagina de usuario

---
+++ usuario.js
@@ -0,0 +1,27 @@
+var userName = document.getElementById('userName').value;
+var userPw = document.getElementById('userPw').value;
+
+// storing input from register-form
+function store() {
+    localStorage.setItem('name', name.value);
+    localStorage.setItem('pw', pw.value);
+}
+
+// check if stored data from register-form is equal to entered data in the   login-form
+function check() {
+
+    // stored data from the register-form
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    // entered data from the login-form
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+
+    // check if stored data from register-form is equal to data from login form
+    if(userName.value == storedName && userPw.value == storedPw) {
+        alert('You are loged in.');
+    }else {
+        alert('ERROR.');
+    }
+}

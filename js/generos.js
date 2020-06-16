8e274fed5621cd52fa3d3039c7877e2c45b156d1
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-16 00:51:36 -0300

    scroll button terminado todas las paginas

--- js/generos.js
+++ js/generos.js
@@ -42,7 +42,6 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari

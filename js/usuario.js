b25de911dd065f577ee1ddda6b0d4fd7ab13b0fa
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-16 16:54:57 -0300

    css a resultados

--- js/usuario.js
+++ js/usuario.js
@@ -25,3 +25,13 @@ function check() {
         alert('ERROR.');
     }
 }
+
+//usar submit- para el formulario, add event listener, el evento es submit, ahi dispara 
+//la funcion store + la linea 1 y 2 adentro de la funcion store
+//check otro event listener en el log in
+//al primero un event listener de store y otro a check
+//validar para que no este vacio
+//2 opciones- 1 html atributos para validar que no este vacio-2 javascript check si value.length es mayor a 0
+//user un objeto que tiene name y password- user.name + user.password
+//cuando guarde un tema, atraer a ese user y agregarle un posicion playlist de un array con los ids que haya
+//

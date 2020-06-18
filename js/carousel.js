69941c22d9de5e101120f4610336983a24b4b477
Niky <NikyB@MacBook-Pro-2.fibertel.com.ar>
2020-06-18 03:26:07 -0300

    java carousel automatico

--- js/carousel.js
+++ js/carousel.js
@@ -66,6 +66,21 @@ nextButton.addEventListener('click', e => {
 //move to next slide//
  });
 
+ var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName(".current-slide");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
 
 
 //cuando aprieto para la izq que se mueva, y vice versa
